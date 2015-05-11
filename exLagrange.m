3ccf6dfcf2cb2dac8913c56ddc8dde40fedeb950
Nano Molina <nanomolinacav@gmail.com>
2015-05-11 02:30:57 -0300

    lab terminado

--- exLagrange.m
+++ exLagrange.m
@@ -2,11 +2,17 @@ function exLagrange()
     x = 0:10;
     y = [];
     z = [];
+    
     for i=0:10
         y(end + 1) = 1/(i**3 + 1);
-        z(end + 1) = i + 1/2;
     endfor
+    z = 0.5:1:10.5;
+    
     w = ilagrange(x, y, z);
-    disp(w)
-    plot(x, y, "r--.")
+    
+    plot(z, w, ".");
+    axis([0, 1], [0, 11])
+    legend("1/(x^3 + 1)")
+    title ("Método de Lagrange");
+    grid on
 end
